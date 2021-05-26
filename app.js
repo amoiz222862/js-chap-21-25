7a7ed8bd91994b569b7bd48abc102b54df55d24b
49985 <amoiz222862@gmail.com>
2021-05-26 11:53:20 +0500

    complete21-25

---
+++ app.js
@@ -0,0 +1,119 @@
+// TASK 1
+// var firstNAME = prompt("enter your first name");
+// var lastNAME = prompt("enter your last name");
+// var fullNAME = firstNAME + " " + lastNAME;
+// document.write("welcome" + " " +fullNAME);
+
+
+// TASK 2
+// var favMOBILE = prompt("Enter your favorite mobile name");
+// document.write(favMOBILE.length);
+
+
+// Task 3
+
+// var name = ["pakistani"];
+// console.log(name.indexOf("n"))
+
+
+
+
+// TASK 4
+
+// var name = ["HELLO WORLD"];
+// document.write(name.lastIndexOf("L"));
+
+
+
+// TASK 5
+
+// var name = ["pakistani"];
+// document.write(name.charAt("3"));
+
+
+// TASK 6
+
+// var firstNAME = "Abdul Moiz";
+// var lastNAME = "Zahid";
+// document.write(firstNAME.concat(" " + lastNAME))
+
+
+// TASK 7
+
+// var name = "HYDERABAD";
+// var newName = name.replace("HYDER" , "ISLAM");
+// document.write(newName)
+
+
+// TASK 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together." ;
+// var placeWord = message.replace(/and/g, "&" );
+// document.write(placeWord);
+
+
+// TASK 9
+
+// var number = "427"
+// var newNumber =  Number(427);
+// console.log(newNumber);
+
+
+
+// TASK 10
+
+// var name = "peanuts";
+// var newName = name.toUpperCase();
+// document.write(newName);
+
+
+
+// TASK 11
+
+// var name = "javascript";
+// var newName = name.slice(0,1).toUpperCase()
+// document.write(newName + "avascript")
+
+
+
+// TASK 12
+
+
+// TASK 13
+
+
+// var a = [];
+// a = prompt("enter a name");
+// for (var i = 0; i < a.length; i++) {
+//        if (a[i] === "!" || a[i] === "@" || a[i] === "#" || a[i] === "$") {
+//               alert("ENTER a valid username : does not contain special symbols e.g !@%$ ");
+//               break;
+
+//        }
+// }
+
+
+
+// TASK 16
+
+// var a = "university of karachi";
+// document.write(a + " " + "<br>")
+// var b = []
+//  b = a.split("");
+//  for (var i =0; i<b.length; i++){
+//     document.write(b[i] + "<br>" );
+//  }
+
+
+// TASK 17
+
+// var a = [];
+// a = prompt("enter");
+// var i;
+// for (i = a.length-1; i>=0; i--){
+//     document.write("the last character of " + a + " " + a[i]);
+//     break;   
+// }
+
+
+
